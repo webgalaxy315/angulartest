cf36be2e80ea9834d348bcc739cd99d876a16aac
ZengGang315 <zoelasa1111@gmail.com>
2022-08-28 18:15:39 +0900

    feat: complete filter box ui

--- src/app/pages/video-game/video-game.module.ts
+++ src/app/pages/video-game/video-game.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { VideoGameRoutingModule } from './video-game-routing.module';
+
 import { VideoGameComponent } from './video-game.component';
 import { FilterComponent } from './filter/filter.component';
 import { VideoGameListItemComponent } from './video-game-list-item/video-game-list-item.component';
 
 @NgModule({
   declarations: [VideoGameComponent, FilterComponent, VideoGameListItemComponent],
-  imports: [CommonModule, VideoGameRoutingModule]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, VideoGameRoutingModule]
 })
 export class VideoGameModule {}
